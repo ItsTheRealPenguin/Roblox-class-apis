39ad45a72bf84e7de1bec7556cbb882909184ca6
ItsTheRealPenguin <141349767+ItsTheRealPenguin@users.noreply.github.com>
2024-04-28 21:15:28 +0200

    Add files via upload

---
+++ main/PlayerService.lua
@@ -0,0 +1,76 @@
+--[[
+    module
+    * PlayerService
+    
+    made by
+    * itspenguin.
+    * last updated 04/28/24
+]]
+--// Services
+local Players = game:GetService("Players")
+
+
+--// Classes
+local PlayerService = {}
+PlayerService.__index = PlayerService
+
+
+--// Initialize
+function PlayerService.new()
+    local self = setmetatable({}, PlayerService)
+
+    self.Player = Players.LocalPlayer
+    self.Character = self.Player.Character or self.Player.CharacterAdded:Wait()
+    self.Humanoid = (self.Character and self.Character:WaitForChild("Humanoid"))
+
+    self.UserId = self.Player.UserId
+    self.AccountAge = self.Player.AccountAge
+
+    return self
+end
+function PlayerService:Get()
+    return self.new()
+end
+
+
+--// Player model methods
+function PlayerService.fetchPlayerModel(self)
+    self.Character = self.Character or self.Player.CharacterAdded:Wait()
+    return self.Character
+end
+function PlayerService:GetCharacter(): Model
+    return self:fetchPlayerModel()
+end
+function PlayerService:GetHumanoid(): Humanoid
+    return self:GetCharacter():WaitForChild("Humanoid")
+end
+function PlayerService:LoadCharacter()
+    self.Player:LoadCharacter()
+end
+
+
+--// Experience methods
+function PlayerService:Quit()
+    game:Shutdown()
+end
+function PlayerService:Kick(reason: string)
+    self.Player:Kick(reason)
+end
+
+
+--// Player methods
+function PlayerService:GetPlayer(): Player
+    return self.Player
+end
+function PlayerService:GetId(): number
+    return self.UserId
+end
+function PlayerService:GetAge(): number
+    return self.AccountAge
+end
+function PlayerService:FakeAge(age: number)
+    self.Player:SetAccountAge(age)
+end
+
+
+return PlayerService
